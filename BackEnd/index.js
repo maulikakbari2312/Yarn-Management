5ca8245d01b44bec142fe554e07e0e0b62d93474
Maulik Akbari <dev.maulikakbari@gmail.com>
2024-07-08 21:14:24 +0530

    feat: handle cors error

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -6,7 +6,11 @@ var cors = require("cors");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 require("./server");
-app.use(cors());
+app.use(cors(
+  {
+    origin: "*"
+  }
+));
 app.use("/Images", express.static("./Images"));
 
 function routeSetup() {
